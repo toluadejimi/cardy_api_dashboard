81f837d97c0f8330ede1560511469ebf40e3bfbf
Jimmy <toluadejimi@gmail.com>
2023-05-31 21:13:36 +0100

    jjk

--- core/app/Http/Controllers/UserController.php
+++ core/app/Http/Controllers/UserController.php
@@ -4966,13 +4966,8 @@ class UserController extends Controller
         
         $image_parts = explode(";base64,", $img);
         $image_type_aux = explode("image/", $image_parts[0]);
-        $image_type = $image_type_aux[1];
-        
-        $image_base64 = base64_decode($image_parts[1]);
         $fileName = uniqid() . '.png';
-        
-        $file = $folderPath . $fileName;
-        // Storage::put($file, $image_base64);
+                // Storage::put($file, $image_base64);
 
 
         $mono_file_url = url('') . "/public/upload/verify/$fileName";
