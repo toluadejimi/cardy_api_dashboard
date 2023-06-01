6e332103fc5f45156451a410f220148b79eb9601
Jimmy <toluadejimi@gmail.com>
2023-06-01 12:12:08 +0100

    kklk

--- core/app/Http/Controllers/UserController.php
+++ core/app/Http/Controllers/UserController.php
@@ -4967,6 +4967,7 @@ class UserController extends Controller
         // $file->move(public_path('/upload/verify'), $fileName);
 
 
+        dd($request->all());
         if ($request->hasFile('image')) {
 
             $file = $request->file('image');
