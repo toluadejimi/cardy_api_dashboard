73476b34737efaacf962a21d5aa8d220a899a84b
Jimmy <toluadejimi@gmail.com>
2023-06-19 09:49:52 +0100

    pushhj

--- core/app/Models/Transaction.php
+++ core/app/Models/Transaction.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    protected $table = "transactions";
+    protected $guarded = [];
+
+    public function ddlink()
+    {
+        return $this->belongsTo('App\Models\Paymentlink', 'payment_link');
+    }
+    public function inplan()
+    {
+        return $this->belongsTo('App\Models\Invoice', 'payment_link');
+    }
+    public function sender()
+    {
+        return $this->belongsTo('App\Models\User','sender_id');
+    }
+    public function receiver()
+    {
+        return $this->belongsTo('App\Models\User','receiver_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User','user_id');
+    }
+
 }
