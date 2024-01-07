e90d7b0d40f8bf3e0277521c21fe6a6cd489c713
Jimmy <toluadejimi@gmail.com>
2024-01-07 18:42:01 +0100

    pushhh

--- core/app/Http/Controllers/UserController.php
+++ core/app/Http/Controllers/UserController.php
@@ -6428,7 +6428,9 @@ class UserController extends Controller
             $data['title'] = 'Transactions';
             $user = Auth::guard('user')->user()->id;
             $data['title'] = 'Transactions';
-            $data['all'] = Transactions::where('note', 'LIKE', "%$request->session_id%")->get();
+            $data['all'] = Transactions::where('note', 'LIKE', "%$request->session_id%")
+            ->where('user_id', Auth::id())
+            ->get();
 
             $data['cash_out'] =Transactions::where([
                     'user_id' => Auth::id(),
